98250b01ec9ea6b3505e012d66e3d20c59f6d53e
Manuel Hotz <manuel.hotz@enplotz.de>
2016-03-14 09:41:33 +0100

    New version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='mensa_ukon',
-      version='0.1',
+      version='0.2',
       description='Access the canteen plan of the Uni Konstanz like a sane person.',
       author_email='manuel.hotz@uni-konstanz.de',
       py_modules=['mensa_ukon'],
