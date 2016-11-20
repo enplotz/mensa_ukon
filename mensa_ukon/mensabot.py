42d0d90558c7ff05c463ff46340b244513a5ace0
Manuel Hotz <manuel.hotz@uni-konstanz.de>
2016-11-20 22:12:48 +0100

    Fix broken bot commands

--- mensa_ukon/mensabot.py
+++ mensa_ukon/mensabot.py
@@ -1,16 +1,13 @@
 #! /usr/bin/python
+import datetime
 import logging
 from collections import OrderedDict
 from collections import namedtuple
-
-import datetime
-from typing import cast
 
 import telegram
 from mensa_ukon import Mensa
 from mensa_ukon import settings
 from mensa_ukon.constants import Language
-
 from telegram import ChatAction
 from telegram import Emoji
 from telegram import ParseMode
@@ -18,6 +15,7 @@ from telegram.ext import CommandHandler
 from telegram.ext import Filters
 from telegram.ext import MessageHandler
 from telegram.ext import Updater
+from telegram.ext.dispatcher import run_async
 
 
 class BotError(Exception):
@@ -36,6 +34,12 @@ class ArgumentError(BotError):
 
     def __init__(self, argument):
         super(ArgumentError, self).__init__('Illegal arguments: {}'.format(argument))
+
+
+class NoMealError(BotError):
+    def __init__(self, meal: str):
+        super(NoMealError, self).__init__('No meal found for \'{}\'.'.format(meal.capitalize()))
+
 
 class MensaBot(telegram.Bot):
 
@@ -77,8 +81,8 @@ class MensaBot(telegram.Bot):
         self.updater = Updater(settings.TOKEN, workers=settings.WORKERS)
         self.dp = self.updater.dispatcher
         # Custom command handlers
-        self._add_bot_command('start', self._start, 'start bot')
-        self._add_bot_command('help', self._bot_help, 'display help message')
+        self._add_bot_command('start', lambda bot, update: self._start(bot, update), 'start bot')
+        self._add_bot_command('help', lambda bot, update: self._bot_help(bot, update), 'display help message')
         self._add_bot_command('mensa', lambda bot, update, args: self._mensa_plan_all(update, args=args),
                               self.DATE_HELP, pass_args=True)
         # alias for autocorrected command
@@ -87,10 +91,8 @@ class MensaBot(telegram.Bot):
         for cmd in self.SHORTCUTS:
             self._add_meal_command(cmd)
         # Again telegram bot framework code
-        self.dp.add_error_handler(self.error)
-        self.dp.add_handler(MessageHandler(Filters.command, self.unknown))
-
-
+        self.dp.add_error_handler(error)
+        self.dp.add_handler(MessageHandler(Filters.command, lambda bot, update: self.unknown(bot, update)))
 
     def run(self):
         if settings.USE_POLLING:
@@ -127,7 +129,7 @@ class MensaBot(telegram.Bot):
     def _add_meal_command(self, cmd_shortcut):
         for s in [cmd_shortcut.command, cmd_shortcut.command.capitalize()]:
             self.dp.add_handler(CommandHandler(s,
-                                               lambda bot, update, args: bot._mensa_plan(update,
+                                               lambda bot, update, args: self._mensa_plan(update,
                                                                                          meal=cmd_shortcut.meal,
                                                                                          args=args),
                                                pass_args=True))
@@ -138,27 +140,24 @@ class MensaBot(telegram.Bot):
         ordered_meals = OrderedDict(sorted(meals.items(), key=lambda t: location.order[t[0]] if location.order else t[0]))
         return ordered_meals
 
-    @staticmethod
-    def _print_commands(bot):
+    def _print_commands(self):
         # we want to print both commands for the bot, as well as commands to get meals
-        return "\n".join(map(lambda c: '/' + c[0] + ' ' + c[1], bot.commands)) \
+        return "\n".join(map(lambda c: '/' + c[0] + ' ' + c[1], self.commands)) \
                + '\n' \
-               + "\n".join(map(lambda c: '/' + c.command + ' ' + c.short_help, bot.SHORTCUTS))
+               + "\n".join(map(lambda c: '/' + c.command + ' ' + c.short_help, MensaBot.SHORTCUTS))
 
-    @staticmethod
-    def _start(bot, update):
+    def _start(self, bot, update):
         chat_id = update.message.chat.id
-        bot.sendMessage(chat_id=chat_id, text=bot.GREETING + '\n' + bot.INTRO_COMMANDS + bot.print_commands() + bot.EXAMPLES,
+        bot.sendMessage(chat_id=chat_id, text=self.GREETING + '\n' + self.INTRO_COMMANDS + self._print_commands() + self.EXAMPLES,
                         parse_mode=ParseMode.MARKDOWN,
                         disable_web_page_preview=True)
 
-    @staticmethod
-    def _bot_help(bot, update):
+    def _bot_help(self, bot, update):
         """Prints help text"""
 
         chat_id = update.message.chat.id
-        bot.sendMessage(chat_id=chat_id,
-                        text=bot.INTRO_HELP + bot.INTRO_COMMANDS + bot.print_commands() + bot.EXAMPLES,
+        msg_async(bot=bot, chat_id=chat_id,
+                        text=MensaBot.INTRO_HELP + MensaBot.INTRO_COMMANDS + self._print_commands() + MensaBot.EXAMPLES,
                         parse_mode=ParseMode.MARKDOWN,
                         disable_web_page_preview=True)
 
@@ -168,6 +167,7 @@ class MensaBot(telegram.Bot):
         self._mensa_plan(update, args=args)
 
     def _mensa_plan(self, update, meal=None, args=None):
+        # TODO simplify method...
         # /mensa [today|tomorrow|date]
         # currently, we only support dates* in the args parameter
         chat_id = update.message.chat.id
@@ -190,46 +190,58 @@ class MensaBot(telegram.Bot):
 
             self.logger.debug('Filter for: %s', meal)
             # [(Location, Dict)]
-            meals = self.mensa.retrieve(date, Language.de,
-                              meals=[meal] if meal else None)
+            meals = self.mensa.retrieve(date, Language.de, meals=[meal] if meal else None)
             msg_text = ''
             if meal:
-                # only print the specific locations' meal
-                m = meals[0][1]
-                msg_text = '\n'.join(
-                    ['%s %s:\n' % (Emoji.FORK_AND_KNIFE, date.strftime('%Y-%m-%d')), '*%s:* %s' % m[meal]])
+                # only print the specified meal
+                try:
+                    msg_text = '\n'.join(
+                        ['{0} {1}:\n'.format(Emoji.FORK_AND_KNIFE, date.strftime('%Y-%m-%d')), '*{0}:* {1}'.format(*meals[0][1][meal])])
+                except KeyError:
+                    # there is nothing for the specified meal
+                    raise NoMealError(meal)
             else:
-                # first mensa, then themenpark
                 for loc_meals in meals:
                     if len(loc_meals[1]) > 0:
-                        lines = [
-                            '\n%s %s %s:\n' % (Emoji.FORK_AND_KNIFE, loc_meals[0].nice_name, date.strftime('%Y-%m-%d'))]
-
-                        ordered_meals = self._sort_meals(loc_meals[0], loc_meals[1])
-                        for i in ordered_meals.items():
-                            lines.append('*%s:* %s' % i[1])
-                        msg_text += '\n'.join(lines) + '\n'
+                        msg_text += '\n{0} {1} {2}:\n'.format(
+                        Emoji.FORK_AND_KNIFE, loc_meals[0].nice_name, date.strftime('%Y-%m-%d')) + '\n'.join(
+                            ['*{0}:* {1}'.format(*l[1]) for l in self._sort_meals(loc_meals[0], loc_meals[1]).items()]) + '\n'
                     else:
-                        msg_text += 'No meals found for %s at %s %s\n' % (
-                            date.strftime('%Y-%m-%d'), loc_meals[0].nice_name, Emoji.LOUDLY_CRYING_FACE)
-
-            self.sendMessage(chat_id=chat_id, text=msg_text,
-                            parse_mode=ParseMode.MARKDOWN,
-                            disable_web_page_preview=True)
+                        msg_text += 'No meals found for %s at %s %s\n' % (date.strftime('%Y-%m-%d'), loc_meals[0].nice_name, Emoji.LOUDLY_CRYING_FACE)
+            try:
+                msg_async(bot=self, chat_id=chat_id, text=msg_text,
+                                parse_mode=ParseMode.MARKDOWN,
+                                disable_web_page_preview=True)
+            except Exception as e:
+                exc(self, e)
         except (ValueError, ArgumentError):
-            self.sendMessage(chat_id, text='Usage: /mensa [<date>]')
+            msg_async(bot=self, chat_id=chat_id, text='Usage: /mensa [<date>]')
+        except NoMealError as nme:
+            msg_async(bot=self, chat_id=chat_id, text=nme.message)
 
-    @staticmethod
-    def error(bot, update, err, **kwargs):
-        """ Error handling """
-        bot.logger.error("An error (%s) occurred: %s", type(err), err.message)
-
-    @staticmethod
-    def unknown(bot, update):
-        bot.sendMessage(chat_id=update.message.chat.id, text='Sorry, I do not understand that command.\n')
-        bot.bot_help(update)
-        bot.logger.info('Recieved unknown command: %s', update.message)
+    def unknown(self, bot, update):
+        msg_async(bot=bot, chat_id=update.message.chat.id, text='Sorry, I do not understand that command.\n')
+        self._bot_help(bot, update)
+        bot.logger.info('Received unknown command: %s', update.message)
 
 
+@run_async
+def msg_async(bot, *args, **kwargs):
+    kwargs['timeout'] = 2
+    try:
+        bot.sendMessage(*args, **kwargs)
+    except Exception as e:
+        exc(bot, e)
+
+
+def exc(bot, ex):
+    """Exception handling."""
+    # TODO use logger from MensaBot instance and not Telegram bot instance
+    bot.logger.exception(ex)
+
+
+def error(bot, err):
+    """ Error handling."""
+    bot.logger.error("An error (%s) occurred: %s", type(err), err.message)
 
 
